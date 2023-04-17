e7f761c36ae0da6da9e00446c259d32967f1b160
Tanzila Tania <tanzilatania31@gmail.com>
2023-04-17 20:58:57 +0600

    phoneNumberList

---
+++ phoneNumberList.dart
@@ -0,0 +1,16 @@
+void main() {
+  List <String> phoneNumberList = [
+    '+88',
+    '01768131685',
+    '01768171985',
+    '01768111286',
+    '01768131685'];
+  String fullPhoneNumber1 = '+88' + phoneNumberList[1];
+  String fullPhoneNumber2 = '+88' + phoneNumberList[2];
+  String fullPhoneNumber3 = '+88' + phoneNumberList[3];
+  String fullPhoneNumber4 = '+88' + phoneNumberList[4];
+  print(fullPhoneNumber1);
+  print(fullPhoneNumber2);
+  print(fullPhoneNumber3);
+  print(fullPhoneNumber4);
+}
